56c183ae0bf7e2bc60aab86c52d2f40dfab50253
Prince Verma <princeverma.codes@gmail.com>
2026-07-07 23:24:59 +0530

    binary search

--- Arrays/largestArray.java
+++ Arrays/largestArray.java
@@ -1,14 +1,19 @@
+import java.util.*;
 class largestArray{
 	public static void main(String[] args){
 		int arr[] = {1,2,6,3,5};
 
-		int largest = 0;
-
+		int largest = Integer.MIN_VALUE;
+		int smallest = Integer.MAX_VALUE;
 		for(int i = 0; i < arr.length; i++){
 			if(largest < arr[i]){
 				largest = arr[i];
 			}
+			if(smallest > arr[i]){
+				smallest = arr[i];
+			}
 		}
-		System.out.println(largest);
+		System.out.println("The Largest Value : " + largest);
+		System.out.println("The Smallest Value : " + smallest);
 	}
 }
