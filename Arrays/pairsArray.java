d784c6c5c1492104664ce4c7c57515c820cce81d
Prince Verma <princeverma.codes@gmail.com>
2026-07-11 23:55:38 +0530

    Find Pairs In Array

--- Arrays/pairsArray.java
+++ Arrays/pairsArray.java
@@ -7,7 +7,7 @@ class pairsArray{
 		for(int i = 0; i < arr.length ; i++){
 			for(int j = i+1; j < arr.length ; j++){
 				System.out.print("(" + arr[i] + "," + arr[j] + ")");
-
+				// Printing all pairs
 			}
 			System.out.println();
 		}
