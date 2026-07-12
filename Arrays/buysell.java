357e3cff16554c102cf80386e99ae8979e94c619
Prince Verma <princeverma.codes@gmail.com>
2026-07-12 21:13:13 +0530

    buy and sell stocks at best profit

---
+++ Arrays/buysell.java
@@ -0,0 +1,29 @@
+class buysell {
+		
+	public static int buyandsell(int prices[]){
+
+
+	int buyprice = Integer.MAX_VALUE;
+	int maxProfit =0;
+
+	for (int i =0; i < prices.length; i++){
+		if(buyprice < prices[i]){
+			int profit = prices[i] - buyprice;
+			maxProfit = Math.max(maxProfit, profit);
+		}
+		else {
+			buyprice = prices[i];
+		}
+	}
+		return maxProfit;
+
+
+	}
+
+	public static void main(String[] args){
+		int prices[] = {7,1,5,3,6,4};
+
+		System.out.println(buyandsell(prices));
+	}
+	
+}
