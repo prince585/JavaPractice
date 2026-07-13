163f29ba2739546e6103cd136a9314b904d99f67
Prince Verma <princeverma.codes@gmail.com>
2026-07-13 20:28:21 +0530

    bubble sort

--- Arrays/Sorting/bubbleSort.java
+++ Arrays/Sorting/bubbleSort.java
@@ -1,5 +1,33 @@
-class bubbbleSort{
+class bubbleSort{
+
+
+	public static void bubbleSort(int arr[]){
+		for(int i = 0; i < arr.length-1; i++){
+			for(int j = 0; j < arr.length -1 - i; j++){
+			if(arr[j] < arr[j+1]){
+			int temp = arr[j];
+			arr[j] = arr[j+1];
+			arr[j+1] = temp;
+				}
+			}
+		}
+
+		for(int items : arr){
+			System.out.println(items);
+		}
+	}
+
+
 	public static void main(String[] args){
 		System.out.println("Bubble Sort");
+
+		int arr[] = {5,4,8,2,3,6,9,7,1};
+
+		bubbleSort(arr);
+		
+
+
+
+
 	}
 }
