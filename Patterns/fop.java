900b4455ac4600edc8f4bc6b7a4cc6a137cc2d11
Prince Verma <princeverma.codes@gmail.com>
2026-07-17 15:08:26 +0530

    nvim testing

---
+++ Patterns/fop.java
@@ -0,0 +1,14 @@
+class fop{
+
+	public static void main(String[] args){
+
+	for(int i = 0; i < 5; i += 1){
+		for(int j = 0; j < i; j += 1){
+			System.out.print("@ ");
+		}
+		System.out.println();
+	}
+
+
+	}
+}
