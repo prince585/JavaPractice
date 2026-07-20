fa00c83d1c7328504ee951103a9dace521d69ecb
Prince Verma <princeverma.codes@gmail.com>
2026-07-20 21:11:27 +0530

    new code

--- Patterns/halfPyramid.java
+++ Patterns/halfPyramid.java
@@ -2,7 +2,7 @@ class halfPyramid{
 	public static void main(String[] args){
 		for(int i = 0; i < 5; i++){
 			for(int k = 0; k < i; k++){
-				System.out.print(k + " ");
+				System.out.print(k+1 + " ");
 			}
 			System.out.println();
 		}
---
+++ Patterns/newclass.java
@@ -0,0 +1,22 @@
+class newclass {
+	public static void main(String[] args){
+		System.out.println("Hey How are you doing!");
+
+		int arr[] = {1,3,4,6,8,7};
+
+
+		int maxProfit = 0;
+		int profit = 0;
+		int buyprice = 2;
+		for(int i = 0; i < arr.length; i++){
+			profit = arr[i] - buyprice;
+			if(maxProfit < profit){
+				maxProfit = profit;
+			}
+		}
+
+		System.out.println(maxProfit);
+
+
+	}
+}
