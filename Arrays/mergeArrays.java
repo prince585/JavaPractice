23a550082695ee5b5e3d93072fca75f024cdd65a
Prince Verma <83363663+prince585@users.noreply.github.com>
2026-08-13 09:01:31 +0530

    merge arrays

---
+++ Arrays/mergeArrays.java
@@ -0,0 +1,36 @@
+class mergeArrays {
+    public static void main(String[] args) {
+        int a[] = {1,3,5,7};
+        int b[] = {2,4,6,8};
+        int c[] = new int[a.length + b.length];
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        while(i < a.length && j < b.length){
+            if(a[i] < b[j]){
+                c[k] = a[i];
+                i++;
+            }else{
+                c[k] = b[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i < a.length){
+            c[k] = a[i];
+            i++;
+            k++;
+        }
+        while(j < b.length){
+            c[k] = b[j];
+            j++;
+            k++;
+        }
+
+        for(int item : c){
+            System.out.print(item + " ");
+        }
+    }
+}
