623b9b299813f8cc8d42d92067c05102f354760a
Prince Verma <83363663+prince585@users.noreply.github.com>
2026-08-14 08:59:31 +0530

    left rotate

---
+++ Arrays/leftRotate.java
@@ -0,0 +1,15 @@
+class leftRotate{
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,5,6};
+        int first = arr[0];
+
+        for(int i = 0; i < arr.length - 1; i++){
+            arr[i] = arr[i + 1];
+        }
+        arr[arr.length - 1] = first;
+
+        for(int item : arr){
+            System.out.println(item);
+        }
+    }
+}
