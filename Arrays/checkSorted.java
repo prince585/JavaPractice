e4c11ceaed75229778a3e323b4951b1bbadbccf0
Prince Verma <83363663+prince585@users.noreply.github.com>
2026-08-16 09:01:15 +0530

    check sorted

---
+++ Arrays/checkSorted.java
@@ -0,0 +1,15 @@
+class checkSorted{
+    public static void main(String[] args){
+        int arr[] = {1,2,3,4,6,5};
+        boolean sorted = true;
+
+        for(int i = 0; i < arr.length - 1; i++){
+            if(arr[i] > arr[i+1]){
+                sorted = false;
+                break;
+            }
+        }
+
+        System.out.println(sorted);
+    }
+}
