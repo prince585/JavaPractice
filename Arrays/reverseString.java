3938b3ebeb3fc9d581c87fc7bbaa67f04f0cecae
Prince Verma <83363663+prince585@users.noreply.github.com>
2026-08-16 09:01:37 +0530

    reverse string

---
+++ Arrays/reverseString.java
@@ -0,0 +1,12 @@
+class reverseString{
+    public static void main(String[] args){
+        String str = "java practice";
+        String rev = "";
+
+        for(int i = str.length() - 1; i >= 0; i--){
+            rev = rev + str.charAt(i);
+        }
+
+        System.out.println(rev);
+    }
+}
