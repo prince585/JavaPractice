04bd711f14252a685894f800880009cf2bd5bd18
Prince Verma <83363663+prince585@users.noreply.github.com>
2026-08-20 08:58:57 +0530

    diagonal sum

---
+++ Arrays/diagonalSum.java
@@ -0,0 +1,17 @@
+class diagonalSum{
+
+    public static void main(String[] args){
+        int arr[][] = {
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum = sum + arr[i][i];
+        }
+
+        System.out.println("diagonal sum = " + sum);
+    }
+}
